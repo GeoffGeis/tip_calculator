30170e1a3c34d3468c26d4deacee350e84d02a66
GeoffGeis <geoffgeis@hotmail.com>
2014-02-19 04:27:19 +0000

    tip_calc with truncated outputs

--- tip_calculator2.rb
+++ tip_calculator2.rb
@@ -41,7 +41,7 @@ class TipCalc
       0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.20, 
       0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.30]
     random = output.sample
-    puts "the meal was #{meal_plus_tax}"
+    puts "the meal was $%.2f\n" % meal_plus_tax
     puts "the tip is #{random}"
     puts "the total is $%.2f\n" % [meal_plus_tax + meal_plus_tax * random]
   end
@@ -49,6 +49,10 @@ class TipCalc
   def end_interface
     puts "Shall we do a random tip? (y/n)?"
     input = gets.chomp.to_s.downcase
+    while input != 'y' and input != 'n'
+      puts "please enter 'y' or 'n'"
+      input = gets.chomp.to_s.downcase
+    end
     if input == 'y' 
       random_tip
     end
