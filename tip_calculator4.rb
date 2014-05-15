6c20c4b84d4527877a66aabc2a470b7340936e77
GeoffGeis <geoffgeis@hotmail.com>
2014-05-15 18:00:27 -0500

    improved method order

--- tip_calculator4.rb
+++ tip_calculator4.rb
@@ -1,24 +1,5 @@
 class TipCalc
-  def calculate_rate(base, percentage)
-    amount = base * percentage / 100
-    return amount
-  end
-
-  def print_values
-    3.times do
-      print "/";sleep(0.1);print "\b";print "-";sleep(0.1);print "\b";
-      print "\\";sleep(0.1);print "\b";print "|";sleep(0.1);print "\b"
-    end
-    meal_plus_tax = @meal + calculate_rate(@meal, @tax)
-    total = meal_plus_tax + calculate_rate(meal_plus_tax, @tip)
-    puts "cost of meal $#{@meal}"
-    puts "tax #{@tax.to_i}%"
-    puts "tip #{@tip.to_i}%"
-    puts "total amount due: $%.2f\n" % total
-    gets
-  end
-
-  def grab_user_values
+  def initialize
     puts "Please input the meal cost $0.00"
     @meal = gets.chomp.to_f
     puts "Please input the tax 0%"
@@ -36,14 +17,32 @@ class TipCalc
        puts "input tip 0%"
       @tip = gets.chomp.to_f
     end
+    print_values
   end
 
   def random_tip
     output = (0..30).map { |i| i }
     @tip = output.sample.to_f
   end
+
+  def print_values
+    3.times do
+      print "/";sleep(0.1);print "\b";print "-";sleep(0.1);print "\b";
+      print "\\";sleep(0.1);print "\b";print "|";sleep(0.1);print "\b"
+    end
+    meal_plus_tax = @meal + calculate_rate(@meal, @tax)
+    total = meal_plus_tax + calculate_rate(meal_plus_tax, @tip)
+    puts "cost of meal $#{@meal}"
+    puts "tax #{@tax.to_i}%"
+    puts "tip #{@tip.to_i}%"
+    puts "total amount due: $%.2f\n" % total
+    gets
+  end
+
+  def calculate_rate(base, percentage)
+    amount = base * percentage / 100
+    return amount
+  end
 end
 
 tip_object = TipCalc.new
-tip_object.grab_user_values
-tip_object.print_values
